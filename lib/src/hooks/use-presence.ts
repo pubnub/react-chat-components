9b46268345bcb19c5536a36955e218b97b2a4a93
Przemysław Janowski <przemyslaw.janowski@pubnub.com>
2021-04-29 22:27:21 +0200

    feat: SDK-837 add refetch to usePresence hook

--- lib/src/hooks/use-presence.ts
+++ lib/src/hooks/use-presence.ts
@@ -1,76 +1,91 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useMemo } from "react";
 import { HereNowParameters, HereNowResponse } from "pubnub";
 import { usePubNub } from "pubnub-react";
 import cloneDeep from "lodash.clonedeep";
 
 type ChannelsOccupancy = HereNowResponse["channels"];
-type HookReturnValue = [ChannelsOccupancy, number, Error];
+type HookReturnValue = [ChannelsOccupancy, () => Promise<void>, number, Error];
 
-export const usePresence = (options: HereNowParameters = {}): HookReturnValue => {
+export const usePresence = ({
+  channels,
+  channelGroups,
+  includeUUIDs,
+  includeState,
+}: HereNowParameters = {}): HookReturnValue => {
   const pubnub = usePubNub();
 
-  const [channels, setChannels] = useState<ChannelsOccupancy>({});
+  const [presence, setPresence] = useState<ChannelsOccupancy>({});
   const [error, setError] = useState<Error>();
 
-  const channelValues = Object.values(channels);
-  const total = channelValues.map((ch) => ch.occupancy).reduce((prev, cur) => prev + cur, 0);
+  const presenceValues = Object.values(presence);
+  const total = presenceValues.map((ch) => ch.occupancy).reduce((prev, cur) => prev + cur, 0);
 
-  const command = async () => {
+  const options = useMemo(() => ({ channels, channelGroups, includeUUIDs, includeState }), [
+    channels,
+    channelGroups,
+    includeUUIDs,
+    includeState,
+  ]);
+
+  const command = useCallback(async () => {
     try {
       const response = await pubnub.hereNow(options);
-      setChannels(response.channels);
+      setPresence(response.channels);
     } catch (e) {
       setError(e);
     }
-  };
+  }, [pubnub, options]);
 
-  const handlePresence = (event) => {
-    setChannels((channels) => {
-      const channelsClone = cloneDeep(channels);
-      if (!channelsClone[event.channel]) channelsClone[event.channel] = {};
-      const channel = channelsClone[event.channel];
+  const handlePresence = useCallback(
+    (event) => {
+      setPresence((presence) => {
+        const presenceClone = cloneDeep(presence);
+        if (!presenceClone[event.channel]) presenceClone[event.channel] = {};
+        const channel = presenceClone[event.channel];
 
-      if (event.action === "join") {
-        if (!channel.hasOwnProperty("occupants")) channel.occupants = [];
-        channel.occupancy = event.occupancy;
+        if (event.action === "join") {
+          if (!channel.hasOwnProperty("occupants")) channel.occupants = [];
+          channel.occupancy = event.occupancy;
 
-        if (
-          options.includeUUIDs !== false &&
-          channel.hasOwnProperty("occupants") &&
-          !channel.occupants.find((u) => u.uuid == event.uuid)
-        ) {
-          const { state, uuid } = event;
-          channel.occupants.push({ state, uuid });
+          if (
+            options.includeUUIDs !== false &&
+            channel.hasOwnProperty("occupants") &&
+            !channel.occupants.find((u) => u.uuid == event.uuid)
+          ) {
+            const { state, uuid } = event;
+            channel.occupants.push({ state, uuid });
+          }
         }
-      }
 
-      if (["leave", "timeout"].includes(event.action)) {
-        channel.occupancy = event.occupancy;
+        if (["leave", "timeout"].includes(event.action)) {
+          channel.occupancy = event.occupancy;
 
-        if (
-          options.includeUUIDs !== false &&
-          channel.hasOwnProperty("occupants") &&
-          channel.occupants.find((u) => u.uuid == event.uuid)
-        ) {
-          channelsClone[event.channel].occupants = channel.occupants.filter(
-            (u) => u.uuid !== event.uuid
-          );
+          if (
+            options.includeUUIDs !== false &&
+            channel.hasOwnProperty("occupants") &&
+            channel.occupants.find((u) => u.uuid == event.uuid)
+          ) {
+            presenceClone[event.channel].occupants = channel.occupants.filter(
+              (u) => u.uuid !== event.uuid
+            );
+          }
         }
-      }
 
-      return channelsClone;
-    });
-  };
+        return presenceClone;
+      });
+    },
+    [options.includeUUIDs]
+  );
 
   useEffect(() => {
     pubnub.addListener({ presence: handlePresence });
-  }, []);
+  }, [handlePresence, pubnub]);
 
   useEffect(() => {
-    setChannels({});
+    setPresence({});
     setError(null);
     command();
-  }, [JSON.stringify(options.channels)]);
+  }, [command]);
 
-  return [channels, total, error];
+  return [presence, command, total, error];
 };
