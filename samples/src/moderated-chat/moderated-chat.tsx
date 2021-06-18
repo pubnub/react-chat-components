e08821fa79ae27aa70304539922a4a98fb1204d3
Elvis Wolcott <elvis.wolcott@pubnub.com>
2021-06-18 10:47:15 -0400

    feat: use objects for channels and channel memberships

--- samples/src/moderated-chat/moderated-chat.tsx
+++ samples/src/moderated-chat/moderated-chat.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Picker } from "emoji-mart";
 import DarkModeToggle from "react-dark-mode-toggle";
 import { ChannelMetadataObject, UUIDMetadataObject, ObjectCustom, GetAllMetadataParameters } from "pubnub";
@@ -11,8 +11,10 @@ import {
   MessageList,
   Themes,
   TypingIndicator,
+  useChannelMembers,
   usePresence,
   useUser,
+  useUserMemberships,
   useUsers,
 } from "@pubnub/react-chat-components";
 
@@ -20,36 +22,41 @@ import "./moderated-chat.scss";
 import "emoji-mart/css/emoji-mart.css";
 import { ReactComponent as PeopleGroup } from "../people-group.svg";
 
-/**
- * Data about users and channels is stored with PubNub Objects.
- * */
-import socialChannels from "../../../data/channels-social.json";
-import directChannels from "../../../data/channels-direct.json";
-const socialChannelList: ChannelMetadataObject<ObjectCustom>[] = socialChannels;
-const directChannelList: ChannelMetadataObject<ObjectCustom>[] = directChannels;
-const allChannelIds = [...socialChannelList, ...directChannelList].map((c) => c.id);
-
 interface UserCustom extends ObjectCustom {
   profileUrl: string;
 }
 
-function SimpleChat() {
+function SimpleChat () {
   const pubnub = usePubNub(); //usePubNub is only used here to get current user info (as it's randomly selected)
+  const params = useMemo(() => ({ uuid: pubnub.getUUID(), include: { channelFields: true, customChannelFields: true } }),[]) as unknown as GetAllMetadataParameters;
+  const [channels] = useUserMemberships(params);
+  const [currentUser] = useUser(params) as [UUIDMetadataObject<UserCustom>, Error];
+  if (channels.length < 1 || !currentUser) {
+    return <div></div>
+  }
+  return <SimpleChatContent channels={channels} currentUser={currentUser}/>
+}
+
+function SimpleChatContent({ currentUser, channels }: { channels: ChannelMetadataObject<ObjectCustom>[], currentUser: UUIDMetadataObject<UserCustom> }) {
   const [theme, setTheme] = useState<Themes>("light");
   const [showMembers, setShowMembers] = useState(false);
   const [showChannels, setShowChannels] = useState(true);
+  
+  // memoize these values to prevent re-renders
+  const socialChannelList = useMemo(() => channels.filter(c => c.id.startsWith('space_')), [channels]);
+  const directChannelList = useMemo(() => channels.filter(c => c.id.startsWith('direct_')), [channels]);
+  const allChannelIds = useMemo(() => channels.map(c => c.id), [channels]);
+
   const [presenceData] = usePresence({ channels: allChannelIds }); // usePresence is one of the custom hooks provided by Chat Components
   const [currentChannel, setCurrentChannel] = useState(socialChannelList[0]);
+
+  const [, , membershipCount] = useChannelMembers({ channel: currentChannel.id, limit: 1});
 
   const [usersRaw] = useUsers();
   const users = usersRaw as UUIDMetadataObject<UserCustom>[];
 
-  const presentUUIDs = presenceData[currentChannel.id]?.occupants?.map((o) => o.uuid);
-  const presentUsers = users.filter((u) => presentUUIDs?.includes(u.id));
-
-  // types are broken
-  const params = { uuid: pubnub.getUUID() } as unknown as GetAllMetadataParameters;
-  const [currentUser] = useUser(params) as [UUIDMetadataObject<UserCustom>, Error];
+  const presentUUIDs = presenceData[currentChannel?.id]?.occupants?.map((o) => o.uuid);
+  const presentUsers = useMemo(() => users.filter((u) => presentUUIDs?.includes(u.id)), [presentUUIDs, users]);
 
   /** Rendered markup is a mixture of PubNub Chat Components (Chat, ChannelList, MessageList,
    * MessageInput, MemberList) and some elements to display additional information and to handle
@@ -98,7 +105,7 @@ function SimpleChat() {
             className={`people ${showMembers ? "active" : ""}`}
             onClick={() => setShowMembers(!showMembers)}
           >
-            <span>{presenceData[currentChannel.id]?.occupancy || 0}</span>
+            <span><strong>{presenceData[currentChannel.id]?.occupancy || 0}</strong>{membershipCount ? <span>/{membershipCount}</span> : ""}</span>
             <PeopleGroup />
           </div>
 
