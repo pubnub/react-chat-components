f63c6c38651314639a6af7d3018cd6cc0f5b323c
Przemysław Janowski <przemyslaw.janowski@pubnub.com>
2022-01-18 20:11:35 +0100

    feat(samples): align getting started sample with the guide

--- samples/getting-started/src/getting-started.tsx
+++ samples/getting-started/src/getting-started.tsx
@@ -6,23 +6,23 @@ import { PubNubProvider } from "pubnub-react";
 import { Chat, MessageList, MessageInput } from "@pubnub/react-chat-components";
 
 /* Creates and configures your PubNub instance. Be sure to replace "myPublishKey" and "mySubscribeKey"
-   with your own keyset. If you wish, modify the default "MyFirstUser" UUID value for the chat user. */
+  with your own keyset. If you wish, modify the default "myFirstUser" uuid value for the chat user. */
 const pubnub = new PubNub({
   publishKey: "myPublishKey",
   subscribeKey: "mySubscribeKey",
-  uuid: "MyFirstUser",
+  uuid: "myFirstUser",
 });
-const currentChannel = "MyFirstChannel";
+const currentChannel = "Default";
 const theme = "light";
 
-function GettingStarted(): JSX.Element {
+function App(): JSX.Element {
   return (
     <PubNubProvider client={pubnub}>
       {/* PubNubProvider is a part of the PubNub React SDK and allows you to access PubNub instance
-      in components down the tree. */}
+        in components down the tree. */}
       <Chat {...{ currentChannel, theme }}>
         {/* Chat is an obligatory state provider. It allows you to configure some common component
-        options, like the current channel and the general theme for the app. */}
+          options, like the current channel and the general theme for the app. */}
         <MessageList />
         <MessageInput />
       </Chat>
@@ -30,4 +30,4 @@ function GettingStarted(): JSX.Element {
   );
 }
 
-export default GettingStarted;
+export default App;
