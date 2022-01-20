b673fd7890965c87523fee7a00356a62da735954
Przemysław Janowski <przemyslaw.janowski@pubnub.com>
2022-01-20 12:50:25 +0100

    fix(lib): sending whitespace-only messages UI-372

--- lib/src/message-input/message-input.tsx
+++ lib/src/message-input/message-input.tsx
@@ -96,13 +96,17 @@ export const MessageInput: FC<MessageInputProps> = (props: MessageInputProps) =>
     }, 0);
   };
 
+  const isValidInputText = () => {
+    return !!text.trim().length;
+  };
+
   /*
   /* Commands
   */
 
   const sendMessage = async () => {
     try {
-      if (!file && !text) return;
+      if (!file && !isValidInputText()) return;
       setLoader(true);
 
       if (file) {
@@ -303,7 +307,7 @@ export const MessageInput: FC<MessageInputProps> = (props: MessageInputProps) =>
 
         {!props.hideSendButton && !props.disabled && (
           <button
-            className={`pn-msg-input__send ${text.length && "pn-msg-input__send--active"}`}
+            className={`pn-msg-input__send ${isValidInputText() && "pn-msg-input__send--active"}`}
             disabled={loader || props.disabled}
             onClick={() => sendMessage()}
             title="Send"
