00999b11621e473ba0aeb0cf512d84d4aa2ef0d3
Piotr Suwala <piotr.suwala@pubnub.com>
2022-10-13 13:15:17 +0200

    chore: remove unnecessary files

--- samples/getting-started/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  envDir: path.resolve(__dirname, ".."),
-  envPrefix: "REACT_APP_",
-});
