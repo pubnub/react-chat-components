8a6b6fd83677dbcec561f0300d7b560ce8ae247d
Przemek Janowski <przemyslaw.janowski@pubnub.com>
2022-11-14 14:33:51 +0100

    feat(samples): cross-platform telehealth improvements (#83)
    * feat(samples): cross-platform telehealth improvements
    
    * chore: fix PR comments

--- samples/react/telehealth/src/views/DoctorView.tsx
+++ samples/react/telehealth/src/views/DoctorView.tsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   UserEntity,
   MessageList,
   MessageInput,
-  MemberList,
+  ChannelList,
   Chat,
+  ChannelEntity,
 } from "@pubnub/react-chat-components";
 
 import { ReactComponent as MedicalIcon } from "../assets/clipboard-medical.svg";
 import { ReactComponent as ArrowUp } from "../assets/arrow-turn-up.svg";
+import memberships from "../data/memberships.json";
+import users from "../data/users.json";
 
 type DoctorViewProps = {
-  patient: UserEntity;
   doctor: UserEntity;
-  channel: string;
 };
 
 function DoctorView(props: DoctorViewProps): JSX.Element {
-  const { patient, doctor, channel } = props;
+  const { doctor } = props;
+  const doctorMemberships = memberships.filter((m) => m.members.includes(doctor.id));
+  const channels = doctorMemberships.reduce((acc: ChannelEntity[], m) => {
+    const patientId = m.members.find((id) => id !== doctor.id);
+    const patient = users.find((u) => u.id === patientId);
+    if (!patient) return acc;
+    return [
+      ...acc,
+      {
+        id: m.channelId,
+        name: patient.name,
+        description: `Patient ID: ${patient.id}`,
+        custom: {
+          profileUrl: patient?.profileUrl,
+        },
+        updated: "",
+        eTag: "",
+      },
+    ];
+  }, []);
+  const [currentChannel, setCurrentChannel] = useState(channels[1] || { id: "default" });
 
   return (
-    <div className="p-5 grow overflow-hidden">
-      <div className="flex justify-end overflow-hidden h-full rounded-xl shadow-xl">
-        <Chat currentChannel={channel} users={[patient, doctor]}>
+    <div className="p-5 overflow-hidden h-[750px] w-[740px] flex flex-col">
+      <header className="pb-2 mb-8 border-b border-solid border-gray-300">
+        <h1 className="text-gray-400 font-bold">Doctor&apos;s Interface</h1>
+        <h2 className="text-gray-400">
+          Logged in as: <strong>{doctor.name}</strong>
+        </h2>
+      </header>
+
+      <div className="flex justify-end overflow-hidden grow rounded-xl shadow-xl">
+        <Chat currentChannel={currentChannel.id} users={users as UserEntity[]}>
           <aside className="flex flex-col">
             <header className="flex items-center h-[70px] pl-4 text-white bg-cyan-700 dark:bg-slate-700 tracking-[2px] uppercase">
               <MedicalIcon className="ml-2 mr-5" />
@@ -30,7 +58,7 @@ function DoctorView(props: DoctorViewProps): JSX.Element {
             </header>
 
             <nav className="bg-gray-100 dark:bg-gray-700 flex flex-col grow w-64">
-              <MemberList members={[patient]} />
+              <ChannelList channels={channels} onChannelSwitched={(ch) => setCurrentChannel(ch)} />
             </nav>
 
             <footer className="bg-gray-300 dark:bg-gray-900 text-gray-800 dark:text-white text-sm px-3 h-[58px] flex items-center">
@@ -47,7 +75,7 @@ function DoctorView(props: DoctorViewProps): JSX.Element {
 
           <section className="flex flex-col grow">
             <header className="flex items-center h-[70px] pl-4 bg-cyan-500 dark:bg-slate-500 dark:text-white text-gray-800 shrink-0">
-              <strong>{patient.name}</strong>
+              <strong>{currentChannel.name}</strong>
             </header>
 
             <article className="flex flex-col grow overflow-hidden">
