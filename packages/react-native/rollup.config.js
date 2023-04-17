619ee624513c3f07569cf023136e3c94767cc55b
Przemysław Janowski <przemyslaw.janowski@pubnub.com>
2023-04-17 17:44:49 +0200

    chore: update packages to work with expo 48

--- packages/react-native/rollup.config.js
+++ packages/react-native/rollup.config.js
@@ -14,7 +14,7 @@ function loadBrowserCryptoModule() {
     name: "browser-crypto-module-resolver",
     resolveId(source) {
       if (source === "./lib/rng") {
-        return "../../node_modules/uuid/lib/rng-browser.js";
+        return "../../node_modules/uuid/dist/rng-browser.js";
       }
       return null;
     },
